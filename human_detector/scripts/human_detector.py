2e07e3d8249414f81e11c13d07f9c39052298ac9
kch4fk <kch4fk@virginia.edu>
2020-04-20 11:36:42 -0400

    Only calls EKF if not Nan depth

--- human_detector/scripts/human_detector.py
+++ human_detector/scripts/human_detector.py
@@ -17,6 +17,8 @@ def get_depth(depth_msg):
     # Try to convert depth image to CV readable image.
     # Initialize the CvBridge class
     global depth
+    global x
+    global y
     bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(depth_msg, "32FC1")
     depth = cv_image[x, y]    # distance to tracked pixels
@@ -49,14 +51,14 @@ def image_callback(img_msg):
     mask_noNoise = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
     # Find contours
-    _, contours, _= cv2.findContours(mask_noNoise, 1, 2)
+    contours, hierarchy = cv2.findContours(mask_noNoise, 1, 2)
 
     # Calc coordiantes of human in image by calculating the centroid of the contour
     M = cv2.moments(contours[0])
     x = int(M['m10']/M['m00'])
     y = int(M['m01']/M['m00'])
 
-#    rospy.loginfo("Hello World, X= "+str(x)+"; Y="+str(y))
+    #rospy.loginfo("Hello World, X= "+str(x)+"; Y="+str(y))
 
     #!!!!!!!!!
     #Convert coordiantes to bearing here
@@ -95,9 +97,9 @@ def image_callback(img_msg):
     Tx0 = Sx0+depth*math.cos(bearing_rad)*math.sin(robot_bearing)+depth*math.sin(bearing_rad)*math.cos(robot_bearing)
     Tx1 = Sx1+depth*math.cos(bearing_rad)*math.cos(robot_bearing)+depth*math.sin(bearing_rad)*math.sin(robot_bearing)
     # Motion and angle of target (differential drive human model)
-    vri = 0 #math.sqrt((pow(Tx0-Txs0, 2))+(pow(Tx1-Txs1, 2)))/0.033
-    vli = 0 #math.sqrt((pow(Tx0-Txs0, 2))+(pow(Tx1-Txs1, 2)))/0.033
-    thk = 0 #math.atan((Txs1-Tx1)/(Txs0-Tx0)) #thkr
+    vri = 0.5 #math.sqrt((pow(Tx0-Txs0, 2))+(pow(Tx1-Txs1, 2)))/0.033
+    vli = 0.5 #math.sqrt((pow(Tx0-Txs0, 2))+(pow(Tx1-Txs1, 2)))/0.033
+    thk = math.atan((Txs1-Tx1)/(Txs0-Tx0)) #thkr
     # Tm and TS (initialize, then use previous values)
     Tm0 = Tms0 #Tmr0
     Tm1 = Tms1 #Tmr1
@@ -106,36 +108,37 @@ def image_callback(img_msg):
     TS2 = TSs2 #TSr2
     TS3 = TSs3 #TSr3
 
-    #filename = "track_human.csv"
-    #f = open(filename, "wa")     ## save data to file
+    filename = "track_human.csv"
+    f = open(filename, "wa")     ## save data to file
     #f.write("depth\tTxr0\tTxr1\tthk\tTmr0\tTmr1\tTSr0\tTSr1\tTSr2\tTSr3\n")  ## column headers
     rospy.wait_for_service('human_prob_motion')
-    try:
-        human_motion = rospy.ServiceProxy('human_prob_motion', HumanProbMotion)
-        resp = human_motion(Sx0, Sx1, Su0, Su1, Tx0, Tx1, vri, vli, thk, Tm0, Tm1, TS0, TS1, TS2, TS3)
-        if math.isnan(depth) == False:
-            print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"%(depth, resp.Txr0, resp.Txr1, resp.thk, resp.Tmr0, resp.Tmr1, resp.TSr0, resp.TSr1, resp.TSr2, resp.TSr3))
-#            f.write(str(depth) + "\t" + str(resp.Txr0) + "\t" + str(resp.Txr1) + "\t" + str(resp.thk) + "\t" + str(resp.Tmr0) + "\t")
-#            f.write(str(resp.Tmr0) + "\t" + str(resp.Tmr1) + "\t" + str(resp.TSr0) + "\t" + str(resp.TSr1) + "\t" + str(resp.TSr2) + "\t")
-#            f.write(str(resp.TSr3) + "\t")
-#            f.write("\n")
+    if math.isnan(depth) == False:
+    
+      try:
+          human_motion = rospy.ServiceProxy('human_prob_motion', HumanProbMotion)
+          resp = human_motion(Sx0, Sx1, Su0, Su1, Tx0, Tx1, vri, vli, thk, Tm0, Tm1, TS0, TS1, TS2, TS3)
+          print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"%(depth, resp.Txr0, resp.Txr1, resp.thk, resp.Tmr0, resp.Tmr1, resp.TSr0, resp.TSr1, resp.TSr2, resp.TSr3))
+          f.write(str(depth) + "\t" + str(resp.Txr0) + "\t" + str(resp.Txr1) + "\t" + str(resp.thk) + "\t" + str(resp.Tmr0) + "\t")
+          f.write(str(resp.Tmr0) + "\t" + str(resp.Tmr1) + "\t" + str(resp.TSr0) + "\t" + str(resp.TSr1) + "\t" + str(resp.TSr2) + "\t")
+          f.write(str(resp.TSr3) + "\t")
+          f.write("\n")
 
-        # Position of robot
-        Sxs0 = resp.Sxr0
-        Sxs1 = resp.Sxr1
-        # Position and orientation of target
-        Txs0 = resp.Txr0
-        Txs1 = resp.Txr1
-        thks = resp.thk
-        # Prob model Tm and TS, update state
-        Tms0 = resp.Tmr0
-        Tms1 = resp.Tmr1
-        TSs0 = resp.TSr0
-        TSs1 = resp.TSr1
-        TSs2 = resp.TSr2
-        TSs3 = resp.TSr3
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+          # Position of robot
+          Sxs0 = resp.Sxr0
+          Sxs1 = resp.Sxr1
+          # Position and orientation of target
+          Txs0 = resp.Txr0
+          Txs1 = resp.Txr1
+          thks = resp.thk
+          # Prob model Tm and TS, update state
+          Tms0 = resp.Tmr0
+          Tms1 = resp.Tmr1
+          TSs0 = resp.TSr0
+          TSs1 = resp.TSr1
+          TSs2 = resp.TSr2
+          TSs3 = resp.TSr3
+      except rospy.ServiceException, e:
+          print "Service call failed: %s"%e
 
     #!!!!!!!!!
     #Publish result
@@ -172,6 +175,10 @@ if __name__ == '__main__':
     TSs1 = 0
     TSs2 = 0
     TSs3 = 5
+
+    x = 0
+    y = 0
+    depth = 1
     
     try:
         main()
