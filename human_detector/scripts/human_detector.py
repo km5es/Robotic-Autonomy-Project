ddeec69840fac007656028b5b58f783e277d61c8
kch4fk <kch4fk@virginia.edu>
2020-04-21 19:36:26 -0400

    added robot control and entropy publisher

--- human_detector/scripts/human_detector.py
+++ human_detector/scripts/human_detector.py
@@ -76,110 +76,157 @@ def image_callback(img_msg):
     kernel = np.ones((5,5),np.uint8)
     mask_noNoise = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-    # Find contours
-    contours, hierarchy = cv2.findContours(mask_noNoise, 1, 2)
 
-    # Calc coordiantes of human in image by calculating the centroid of the contour
-    M = cv2.moments(contours[0])
-    x = int(M['m10']/M['m00'])
-    y = int(M['m01']/M['m00'])
+    try:
+      # Find contours
+      contours, hierarchy = cv2.findContours(mask_noNoise, 1, 2)
+      #_, contours, _ = cv2.findContours(mask_noNoise, 1, 2)
 
-    #rospy.loginfo("X= "+str(x)+"; Y="+str(y))
+      # Calc coordiantes of human in image by calculating the centroid of the contour
+      M = cv2.moments(contours[0])
+      x = int(M['m10']/M['m00'])
+      y = int(M['m01']/M['m00'])
 
-    #!!!!!!!!!
-    #Convert coordiantes to bearing here
-    # Kinect has a field of view of 62x48.6 and resolution of 640x480
-    bearing_deg = (31.0/320.0)*(x-320.0)
-    bearing_rad = (math.pi/180.0)*bearing_deg
-    #!!!!!!!!!
+      #rospy.loginfo("X= "+str(x)+"; Y="+str(y))
 
-    #rospy.loginfo("bearing = "+str(bearing_rad))
+      #!!!!!!!!!
+      #Convert coordiantes to bearing here
+      # Kinect has a field of view of 62x48.6 and resolution of 640x480
+      bearing_deg = (31.0/320.0)*(x-320.0)
+      bearing_rad = (math.pi/180.0)*bearing_deg
+      #!!!!!!!!!
 
-    #!!!!!!!!!
-    #Call EKF service here
-    # Initialize and store state of prob model
-    # Position of robot
-    global Sxs0
-    global Sxs1
-    # Position and orientation of target
-    global Txs0
-    global Txs1
-    global thks
-    # Prob model Tm and TS
-    global Tms0
-    global Tms1
-    global TSs0
-    global TSs1
-    global TSs2
-    global TSs3
+      #rospy.loginfo("bearing = "+str(bearing_rad))
 
-    global followerX
-    global followerY
-    global followerYaw
+      #!!!!!!!!!
+      #Call EKF service here
+      # Initialize and store state of prob model
+      # Position of robot
+      global Sxs0
+      global Sxs1
+      # Position and orientation of target
+      global Txs0
+      global Txs1
+      global thks
+      # Prob model Tm and TS
+      global Tms0
+      global Tms1
+      global TSs0
+      global TSs1
+      global TSs2
+      global TSs3
 
-    # Position of robot
-    Sx0 = followerX
-    Sx1 = followerY
-    # Robot bearing (rad)
-    robot_bearing = followerYaw
-    # Motion of robot
-    Su0 = 0
-    Su1 = 0
-    # Position of target
-    Tx0 = Sx0 + depth*math.sin(robot_bearing+bearing_rad)
-    Tx1 = Sx1 + depth*math.cos(robot_bearing+bearing_rad)
+      global followerX
+      global followerY
+      global followerYaw
 
-    # Motion and angle of target (differential drive human model)
-    vri = 0
-    vli = 0
-    thk = math.atan((Txs0-Tx0)/(Txs1-Tx1)) #thkr
-    # Tm and TS (initialize, then use previous values)
-    Tm0 = Tms0 #Tmr0
-    Tm1 = Tms1 #Tmr1
-    TS0 = TSs0 #TSr0
-    TS1 = TSs1 #TSr1
-    TS2 = TSs2 #TSr2
-    TS3 = TSs3 #TSr3
+      # Position of robot
+      Sx0 = followerX
+      Sx1 = followerY
+      # Robot bearing (rad)
+      robot_bearing = followerYaw
+      # Motion of robot
+      Su0 = 0
+      Su1 = 0
+      # Position of target
+      Tx0 = Sx0 + depth*math.sin(robot_bearing+bearing_rad)
+      Tx1 = Sx1 + depth*math.cos(robot_bearing+bearing_rad)
+ 
+      # Motion and angle of target (differential drive human model)
+      vri = 0
+      vli = 0
+      thk = math.atan((Txs0-Tx0)/(Txs1-Tx1)) #thkr
+      # Tm and TS (initialize, then use previous values)
+      Tm0 = Tms0 #Tmr0
+      Tm1 = Tms1 #Tmr1
+      TS0 = TSs0 #TSr0
+      TS1 = TSs1 #TSr1
+      TS2 = TSs2 #TSr2
+      TS3 = TSs3 #TSr3
 
-    #rospy.loginfo(str(Tx0)+", "+str(Tx1))
+      #rospy.loginfo(str(Tx0)+", "+str(Tx1))
 
-    rospy.wait_for_service('human_prob_motion')
-    if math.isnan(depth) == False:
+      rospy.wait_for_service('human_prob_motion')
+      if math.isnan(depth) == False:
     
-      try:
-          human_motion = rospy.ServiceProxy('human_prob_motion', HumanProbMotion)
-          resp = human_motion(Sx0, Sx1, Su0, Su1, Tx0, Tx1, vri, vli, thk, Tm0, Tm1, TS0, TS1, TS2, TS3)
-          print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"%(depth, resp.Txr0, resp.Txr1, resp.thk, resp.Tmr0, resp.Tmr1, resp.TSr0, resp.TSr1, resp.TSr2, resp.TSr3))
+        try:
+            human_motion = rospy.ServiceProxy('human_prob_motion', HumanProbMotion)
+            resp = human_motion(Sx0, Sx1, Su0, Su1, Tx0, Tx1, vri, vli, thk, Tm0, Tm1, TS0, TS1, TS2, TS3)
+            print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"%(depth, resp.Txr0, resp.Txr1, resp.thk, resp.Tmr0, resp.Tmr1, resp.TSr0, resp.TSr1, resp.TSr2, resp.TSr3))
 
-          # Position of robot
-          Sxs0 = resp.Sxr0
-          Sxs1 = resp.Sxr1
-          # Position and orientation of target
-          Txs0 = resp.Txr0
-          Txs1 = resp.Txr1
-          thks = resp.thk
-          # Prob model Tm and TS, update state
-          Tms0 = resp.Tmr0
-          Tms1 = resp.Tmr1
-          TSs0 = resp.TSr0
-          TSs1 = resp.TSr1
-          TSs2 = resp.TSr2
-          TSs3 = resp.TSr3
+            # Position of robot
+            Sxs0 = resp.Sxr0
+            Sxs1 = resp.Sxr1
+            # Position and orientation of target
+            Txs0 = resp.Txr0
+            Txs1 = resp.Txr1
+            thks = resp.thk
+            # Prob model Tm and TS, update state
+            Tms0 = resp.Tmr0
+            Tms1 = resp.Tmr1
+            TSs0 = resp.TSr0
+            TSs1 = resp.TSr1
+            TSs2 = resp.TSr2
+            TSs3 = resp.TSr3
 
-	  # Differential Entropy
-	  # H(X) = ln(sqrt(((2*pi*e)^nx)|TS|))
-          entropy = math.log((math.pow((2*math.pi*math.e),2))*((TSs0*TSs3)-(TSs1*TSs2)))
-          #print("Entropy: %s" %entropy)
+	    # Differential Entropy
+   	    # H(X) = ln(sqrt(((2*pi*e)^nx)|TS|))
+            entropy = math.log((math.pow((2*math.pi*math.e),2))*((TSs0*TSs3)-(TSs1*TSs2)))
+            #print("Entropy: %s" %entropy)
 
-          track_EKF_f.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"%(depth, resp.Txr0, resp.Txr1, resp.thk, resp.Tmr0, resp.Tmr1, resp.TSr0, resp.TSr1, resp.TSr2, resp.TSr3, entropy))
+            track_EKF_f.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"%(depth, resp.Txr0, resp.Txr1, resp.thk, resp.Tmr0, resp.Tmr1, resp.TSr0, resp.TSr1, resp.TSr2, resp.TSr3, entropy))
 
-      except rospy.ServiceException, e:
-          print "Service call failed: %s"%e
+
+
+            # Construct follower Twist message
+            # Remember that linear.x is velocity in +y direction
+            # linear.y is velocity in -x direction
+    
+            if depth > 1:
+              x_vel = 0.6*math.sin(bearing_rad)
+              y_vel = 0.6*math.cos(bearing_rad)
+            else:
+              x_vel = 0
+              y_vel = 0
+
+            #control_msg.linear.x = y_vel
+            #control_msg.linear.y = (-1)*x_vel
+
+            #controller_pub.publish(control_msg)
+
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+    
+      else:
+        x_vel = 0
+        y_vel = 0
+    
+
+      #!!!!!!!!
+      #Publish result
+      #!!!!!!!!
+      pub_entropy = rospy.Publisher('entropy', Float32, queue_size=10)
+      pub_entropy.publish(entropy)
+
+    except IndexError as e:
+      x_vel = 0
+      y_vel = 0
+      print("Target out of FOV!")
 
     #!!!!!!!!!
-    #Publish result
+    #Publish controls
     #!!!!!!!!!
-    ## publish to /follower/base_controller/cmd_vel for control    
+    ## publish to /follower/base_controller/cmd_vel for control  
+    controller_pub = rospy.Publisher('/follower/base_controller/cmd_vel', Twist, queue_size=10)
+    control_msg = Twist()
+
+
+    control_msg.linear.x = y_vel
+    control_msg.linear.y = (-1)*x_vel
+
+    controller_pub.publish(control_msg)
+
 
 
 def get_human_odom(msg):
